a52562e58d673b974bb11fe64721bed9a47b801d
its-archisman <98551177+its-archisman@users.noreply.github.com>
2022-07-22 12:12:14 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -6,8 +6,9 @@ $email_subject="New Form Submission";
 $email_body="Username: $name.\n".
                 "User Email: $visitor_email.\n".
                     "User Message: $message.\n";
-$to='<mail.archismanbiswas@gmail.com>';
+$to="mail.archismanbiswas@gmail.com";
 $headers="From: $email_from \r\n";
 $headers.="Reply-To: $visitor_email \r\n";
 mail($to,$email_subject,$email_body,$headers);
+header("Location: index.html");
 ?>
