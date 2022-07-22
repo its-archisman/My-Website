45c054400289f1be0215cb64d5ccab9d0d9c664a
its-archisman <98551177+its-archisman@users.noreply.github.com>
2022-07-22 12:21:49 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,14 +1,35 @@
 <?php
-$name=$_POST['name'];
-$visitor_email=$_POST['email'];
-$message=$_POST['message'];
-$email_subject="New Form Submission";
-$email_body="Username: $name.\n".
-                "User Email: $visitor_email.\n".
-                    "User Message: $message.\n";
-$to="mail.archismanbiswas@gmail.com";
-$headers="From: $email_from \r\n";
-$headers.="Reply-To: $visitor_email \r\n";
-mail($to,$email_subject,$email_body,$headers);
-header("Location: index.html");
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+$mailheader = "From:".$name."<".$email.">\r\n";
+
+$recipient = "example@gmail.com";
+
+mail($recipient, $subject, $message, $mailheader) or die("Error!");
+
+echo'
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact form</title>
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@600&family=Poppins&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Thank you for contacting me. I will get back to you as soon as possible!</h1>
+        <p class="back">Go back to the <a href="index.html">homepage</a>.</p>
+        
+    </div>
+</body>
+</html>
+';
+
+
 ?>
