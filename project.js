4dbc48af29496104636814b54d2352d58eb431b5
Archisman Biswas <archisman@Archismans-MacBook-Air.local>
2024-08-02 19:10:26 +0530

    Add collapsible buttons

--- project.js
+++ project.js
@@ -1,4 +1,6 @@
+console.log('JavaScript file loaded');
 function toggleTheme() {
+    console.log("TOggling theme")
     var currentTheme = document.body.getAttribute('data-theme');
     if (currentTheme === 'dark') {
         document.body.setAttribute('data-theme', 'light');
@@ -9,3 +11,20 @@ function toggleTheme() {
 if (!document.body.hasAttribute('data-theme')) {
     document.body.setAttribute('data-theme', 'light');
 }
+document.addEventListener('DOMContentLoaded', function() {
+    var coll = document.getElementsByClassName("collapsible");
+    console.log("HERE")
+    for (var i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.display != "none") {
+                content.style.display = "none";
+                console.log("--------")
+            } else {
+                content.style.display = "block";
+                console.log("FFFFFFFFFFF")
+            }
+        });
+    }
+});
